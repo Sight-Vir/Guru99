60812576420316f3c527f7035fd8169d776f8199
unknown <virajsalunkhe545@gmail.com>
2024-01-19 16:01:16 +0530

    BDD framework created

---
+++ AllFeatureFiles/Guru99.feature
@@ -0,0 +1,5 @@
+Feature: Guru99Test application
+
+  Scenario: Guru99 Registeration Functionality
+    Given User is on register page
+    And User validate register Title
---
+++ src/test/java/TestRunner/TestRunnerClass.java
@@ -0,0 +1,14 @@
+package TestRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions
+(features= {"AllFeatureFiles"},
+glue= {"Steps"}
+)
+
+public class TestRunnerClass extends AbstractTestNGCucumberTests{
+	
+
+}
